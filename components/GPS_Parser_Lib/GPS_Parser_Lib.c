2fce4dd2cce82965ae279560c053e65e9db22d06
Azhar Khan <azhar987k@gmail.com>
2023-03-27 21:03:39 +0500

    Sliced String
    Sliced string based on comma , between the paramaters and store the parameters in an array.

--- components/GPS_Parser_Lib/GPS_Parser_Lib.c
+++ components/GPS_Parser_Lib/GPS_Parser_Lib.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "GPS_Parser_Lib.h"
 
-GPS_Data parse_packet(char packet[]){
-    
+GPS_Data parse_gps_data(char packet[]){
+    char params[NUM_PARAMS][LENGTH_PARAMS];
+    char *sliced_string;
+    sliced_string = strtok(packet, ",");
+    int i = 0;
+    while(sliced_string != NULL){
+        params[i][LENGTH_PARAMS] = sliced_string;
+        sliced_string = strtok(NULL, ",");
+        i++;
+    }
+
 }
