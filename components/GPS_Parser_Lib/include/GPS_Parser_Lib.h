3c3b190f486c24ce0549bfca81e0b0184a59c5be
Azhar Khan <azhar987k@gmail.com>
2023-03-27 20:21:01 +0500

    Inital Commit
    Created a struct GPS_Data to store the GPS Data, and created a function protoype to parse the data

---
+++ components/GPS_Parser_Lib/include/GPS_Parser_Lib.h
@@ -0,0 +1,13 @@
+#ifndef GPS_PARSER_LIB_H
+#define GPS_PARSER_LIB_H
+#include<string.h>
+//A struct to store GPS Data
+typedef struct{
+    char time[9], latitude[9], lat_direction[1],
+    longitude[10], long_direction[1], reference_Station_ID[4];
+    int GPS_fix_quality, no_of_satellites, checksum;
+    float altitude, Geoid_separation, differential_age;
+} GPS_Data;
+//A function which takes String as input and returns the GPS_Data struct 
+GPS_Data parse_packet(char packet[]);
+#endif
