b7ac8b9567b3291099b2ace9df56aad8c2a2b53a
Azhar Khan <azhar987k@gmail.com>
2023-03-28 16:49:29 +0500

    Split String
    Split the whole GGA string into different parameters based on Comma

--- components/GPS_Parser_Lib/GPS_Parser_Lib.c
+++ components/GPS_Parser_Lib/GPS_Parser_Lib.c
@@ -2,14 +2,17 @@
 #include "GPS_Parser_Lib.h"
 
 GPS_Data parse_gps_data(char packet[]){
-    char params[NUM_PARAMS][LENGTH_PARAMS];
-    char *sliced_string;
-    sliced_string = strtok(packet, ",");
-    int i = 0;
-    while(sliced_string != NULL){
-        params[i][LENGTH_PARAMS] = sliced_string;
+    GPS_Data data;
+    data.altitude= 0; data.checksum =0; 
+    char *params[NUM_PARAMS];
+    char *sliced_string = strtok(packet, ",");
+    int i = 0;    
+    while (sliced_string != NULL && i < NUM_PARAMS) {
+        params[i++] = sliced_string;
         sliced_string = strtok(NULL, ",");
-        i++;
     }
-
+    for(int i = 0; i<NUM_PARAMS; i++){
+        printf("%s \n", params[i]);
+    }
+    return data;
 }
