3071e46103c38ec1f3b6d32510457da7a705141a
Azhar Khan <azhar987k@gmail.com>
2023-03-28 16:50:42 +0500

    Data Test
    Send a sample GGA message to the functino parse_gps_data

--- main/Unit_Test_GPS_Parser.c
+++ main/Unit_Test_GPS_Parser.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+#include "GPS_Parser_Lib.h"
 void app_main(void)
-{
-
+{   
+    while(1){
+        char packet[] = "$GPGGA,002153.000,3342.6618,N,11751.3858,W,1,10,1.2,27.0,M,-34.2,M,,0000*5E";
+        parse_gps_data(packet);
+        printf("dataa received");
+    }
 }
