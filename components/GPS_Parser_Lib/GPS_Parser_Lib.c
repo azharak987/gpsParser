6dbca31992b0b1dbe608ecb9252fda275f34594d
Azhar Khan <azhar987k@gmail.com>
2023-03-29 15:56:55 +0500

    Added Empty String handling

--- components/GPS_Parser_Lib/GPS_Parser_Lib.c
+++ components/GPS_Parser_Lib/GPS_Parser_Lib.c
@@ -2,17 +2,31 @@
 #include "GPS_Parser_Lib.h"
 
 GPS_Data parse_gps_data(char packet[]){
+    //Struct GPS_Data to store the data
     GPS_Data data;
     data.altitude= 0; data.checksum =0; 
+    //For getting the comma separated strings
     char *params[NUM_PARAMS];
-    char *sliced_string = strtok(packet, ",");
-    int i = 0;    
-    while (sliced_string != NULL && i < NUM_PARAMS) {
-        params[i++] = sliced_string;
-        sliced_string = strtok(NULL, ",");
+    if(!strcmp(packet, "") || packet == NULL){
+        printf("Empty Packet");
+        return;
     }
-    if(!strcmp(params[0], "$GPGGA")){
-        printf("Valid Data");
+    else{
+        //Sliced string based on Comma
+        char *sliced_string = strtok(packet, ",");
+        int i = 0;
+        //Splitting the packet and then storing it in params.
+        while (sliced_string != NULL && i < NUM_PARAMS) {
+            params[i++] = sliced_string;
+            sliced_string = strtok(NULL, ",");
+        }
+        //Comparing the first substring with the packet type to check validity.
+        if(!strcmp(params[0], "$GPGGA")){
+            printf("Valid Data");
+        }
+        else{
+            return;
+        }
     }
     for(int i = 0; i<NUM_PARAMS; i++){
         printf("%s \n", params[i]);
