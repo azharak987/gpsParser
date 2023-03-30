d0998fc4795b687d26c76fb2c77742756abddb55
Azhar Khan <azhar987k@gmail.com>
2023-03-30 10:35:57 +0500

    Changed Datatyes of variables

--- components/GPS_Parser_Lib/include/GPS_Parser_Lib.h
+++ components/GPS_Parser_Lib/include/GPS_Parser_Lib.h
@@ -4,10 +4,10 @@
 #include<string.h>
 //A struct to store GPS Data
 typedef struct{
-    char time[9], lat_direction[1],
-    longitude[10], long_direction[1], reference_Station_ID[4];
+    char time[9], lat_direction,
+    long_direction, reference_Station_ID[4];
     int GPS_fix_quality, no_of_satellites, checksum;
-    float altitude, Geoid_separation, differential_age, latitude;
+    float altitude, Geoid_separation, differential_age, latitude, longitude;
 } GPS_Data;
 //A function which takes String as input and returns the GPS_Data struct 
 GPS_Data parse_gps_data(char packet[]);
