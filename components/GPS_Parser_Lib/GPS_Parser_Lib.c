d323b853f6cdf2128ac85866b10631ee1be9ec7c
Azhar Ali Khan <azhar987k@gmail.com>
2023-04-05 20:45:01 +0500

    Updated the Check Sum Block

--- components/GPS_Parser_Lib/GPS_Parser_Lib.c
+++ components/GPS_Parser_Lib/GPS_Parser_Lib.c
@@ -115,7 +115,10 @@ GPS_Data parse_gps_data(char packet[]){
                 }
             }
             else{
-                strcpy(data.reference_Station_ID, "Missing");
+                printf("Check Sum Missing");
+                strcpy(data.reference_Station_ID, STR_MISSING_CODE);
+                data.checksum = INT_FLOAT_MISSING_CODE;
+                printf(data.reference_Station_ID);
             }
         }
         else{
