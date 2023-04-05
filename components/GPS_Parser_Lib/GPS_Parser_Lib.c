3d4ea2293644e0a55c6d257af100ef53cc5076e0
Azhar Ali Khan <azhar987k@gmail.com>
2023-04-05 09:41:26 +0500

    Identified Missing Parameters
    Identified Missing parameters and added Missing in place of missing parameter.

--- components/GPS_Parser_Lib/GPS_Parser_Lib.c
+++ components/GPS_Parser_Lib/GPS_Parser_Lib.c
@@ -7,6 +7,19 @@ GPS_Data parse_gps_data(char packet[]){
     data.altitude= 0; data.checksum =0; 
     //For getting the comma separated strings
     char *params[NUM_PARAMS];
+    int params_count = 0, missing_params = 15;   
+    int missing_params_pos[15];
+    int empty_params = 0;
+    //Identifying empty packets and storing their position in array.
+    for (int i = 0; i < strlen(packet); i++) {
+        if(packet[i] == ','){
+            params_count++;
+        }
+        if (packet[i] == ',' && packet[i+1] == ',') {
+            missing_params_pos[empty_params] = params_count;
+            empty_params++;
+        }
+    }
     if(!strcmp(packet, "") || packet == NULL){
         printf("Empty Packet");
         return;
@@ -14,9 +27,14 @@ GPS_Data parse_gps_data(char packet[]){
     else{
         //Sliced string based on Comma
         char *sliced_string = strtok(packet, ",");
-        int i = 0;
+        int i = 0, j = 0;
         //Splitting the packet and then storing it in params.
         while (sliced_string != NULL && i < NUM_PARAMS) {
+            //Adding "Missing", in place of missing parameter.
+            if(i == missing_params_pos[j]){
+                params[i] = (char*)"Missing";
+                i++;j++;
+            }
             params[i++] = sliced_string;
             sliced_string = strtok(NULL, ",");
         }
