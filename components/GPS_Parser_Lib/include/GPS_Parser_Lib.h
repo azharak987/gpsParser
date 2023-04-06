1b1031ff1e686d58eb0783175bf5406fcc2056f6
Azhar Ali Khan <azhar987k@gmail.com>
2023-04-06 15:48:46 +0500

    Updated the Data in the struct.

--- components/GPS_Parser_Lib/include/GPS_Parser_Lib.h
+++ components/GPS_Parser_Lib/include/GPS_Parser_Lib.h
@@ -1,16 +1,30 @@
 #ifndef GPS_PARSER_LIB_H
 #define GPS_PARSER_LIB_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
 #define NUM_PARAMS 14
 #define INT_FLOAT_MISSING_CODE -111
 #define STR_MISSING_CODE "MIS"
 #define CHAR_MISSING_CODE 'M'
-#include<string.h>
+
 //A struct to store GPS Data
-typedef struct{
-    char time[9], lat_direction,
-    long_direction, reference_Station_ID[4];
-    int GPS_fix_quality, no_of_satellites, checksum;
-    float altitude, Geoid_separation, differential_age, latitude, longitude, hdop;
+typedef struct {
+    char time[9];
+    char lat_direction;
+    char long_direction;
+    char reference_Station_ID[4];
+    int GPS_fix_quality;
+    int no_of_satellites;
+    int checksum;
+    float altitude;
+    float Geoid_separation;
+    float latitude;
+    float longitude;
+    float hdop;
+    float differential_age;
+    bool checksum_integrity;
 } GPS_Data;
 //A function which takes String as input and returns the GPS_Data struct 
 GPS_Data parse_gps_data(char packet[]);
