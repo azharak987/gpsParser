5a5758fdaceaddc93c13d3b7c31b66a25360f22b
Azhar Ali Khan <azhar987k@gmail.com>
2023-04-06 15:56:07 +0500

    Added Condiditions for checking missing parameters.
    The conditions checks if the data in the params[] is not equal to Missing, if not equal, then assigning the data to GPS_data struct variable.

--- components/GPS_Parser_Lib/GPS_Parser_Lib.c
+++ components/GPS_Parser_Lib/GPS_Parser_Lib.c
@@ -71,52 +71,100 @@ GPS_Data parse_gps_data(char packet[]){
         }
         //Comparing the first substring with the packet type to check validity.
         if(!strcmp(params[0], "$GPGGA")){
-            printf("Valid Data. \n");
+            //Printf Statement
             //Time
-            //Extract time from the data.
-            int hours = (atof(params[1]))/10000;
-            int mins = (atof(params[1])-(hours*10000))/100;
-            int secs = (atof(params[1])-((hours*10000) + (mins*100)));
-            //Join Hours, Mins, and Secs into one string.
-            sprintf(data.time, "HH:MM:SS: %d:%d:%d", hours, mins, secs);
-            //Tested Time.
-
+            if(strcmp(params[1], "Missing")){
+                //Extract time from the data.
+                int hours = (atof(params[1]))/10000;
+                int mins = (atof(params[1])-(hours*10000))/100;
+                int secs = (atof(params[1])-((hours*10000) + (mins*100)));
+                //Join Hours, Mins, and Secs into one string.
+                sprintf(data.time, "HH:MM:SS: %d:%d:%d", hours, mins, secs);
+                //Tested Time.
+            }
+            else{
+                strcpy(data.time, STR_MISSING_CODE);
+            }
             //Latitude
-            //Extract Latitude in degrees from the data
-            int degrees = (atof(params[2]))/100;
-            //Converting minutes into degrees
-            float mins_to_degrees= ((atof(params[2]))-degrees*100)/60;
-            //Storing in GPS_Data
-            data.latitude = degrees+mins_to_degrees;
+            if(strcmp(params[2], "Missing")){
+                //Extract Latitude in degrees from the data
+                int degrees = (atof(params[2]))/100;
+                //Converting minutes into degrees
+                float mins_to_degrees= ((atof(params[2]))-degrees*100)/60;
+                //Storing in GPS_Data
+                data.latitude = degrees+mins_to_degrees;
+            }
+            else{
+                data.latitude = INT_FLOAT_MISSING_CODE;
+            }
             //Latitude Direction
-            data.lat_direction = *params[3];
-            //Tested Latitude
-            
+            if(strcmp(params[3], "Missing")){
+                data.lat_direction = *params[3];
+            }
+            else{
+                data.lat_direction = CHAR_MISSING_CODE;
+            }
             //Longitude 
-            //Extract Longitude in degrees from the data
-            degrees = (atof(params[4]))/100;
-            //Converting minutes into degrees
-            mins_to_degrees= ((atof(params[4]))-degrees*100)/60;
-            //Storing in GPS_Data
-            data.longitude = degrees+mins_to_degrees;
+            if(strcmp(params[4], "Missing")){
+                //Extract Longitude in degrees from the data
+                int degrees = (atof(params[4]))/100;
+                //Converting minutes into degrees
+                float mins_to_degrees= ((atof(params[4]))-degrees*100)/60;
+                //Storing in GPS_Data
+                data.longitude = degrees+mins_to_degrees;
+            }
+            else{
+                data.longitude = INT_FLOAT_MISSING_CODE;
+            }
             //Longitude Direction
-            data.long_direction = *params[5];
-            //Tested Longitude
-
+            if(strcmp(params[5], "Missing")){
+                data.long_direction = *params[5];
+            }
+            else{
+                data.long_direction = 'M';
+            }
             //GPS Quality Indicator
-            data.GPS_fix_quality = atoi(params[6]);
-            
+            if(strcmp(params[6], "Missing")){
+                data.GPS_fix_quality = atoi(params[6]);
+            }
+            else{
+                data.GPS_fix_quality = INT_FLOAT_MISSING_CODE;
+            }
             //Number of Satelites used
-            data.no_of_satellites = atoi(params[7]);
+            if(strcmp(params[7], "Missing")){
+                data.no_of_satellites = atoi(params[7]);
+            }
+            else{
+                data.no_of_satellites = INT_FLOAT_MISSING_CODE;
+            }
+            //HDOP
+            if(strcmp(params[8], "Missing")){
+                data.hdop = atof(params[8]);
+            }
+            else{
+                data.hdop = INT_FLOAT_MISSING_CODE;
+            }
 
-            //Horizontal Dilution of Precision
-            data.hdop = atof(params[8]);
-
+            //Altitude
+            if(strcmp(params[9], "Missing")){
+                data.altitude = atof(params[9]);
+            }
+            else{
+                data.altitude = INT_FLOAT_MISSING_CODE;
+            }
             //Geoid Separation
-            data.Geoid_separation = atof(params[11]);
-            //Getting Differential Age
+            if(strcmp(params[11], "Missing")){
+                data.Geoid_separation = atof(params[11]);
+            }
+            else{
+                data.Geoid_separation = INT_FLOAT_MISSING_CODE;
+            }
+            //Differential Age
             if(strcmp(params[13], "Missing")){
-                data.differential_age = atoi(params[13]);
+                data.differential_age = atof(params[13]);
+            }
+            else{
+                data.differential_age = INT_FLOAT_MISSING_CODE;
             }
             //Getting Check sum and Reference Station ID
             //If the data is present in the packet
